9fed7e9649d162bb5a2ba728b7e5be9e403ade41
rodinka7 <rodinka7@gmail.com>
2016-10-16 22:01:00 +0300

    Создание JS части

--- js/main.js
+++ js/main.js
@@ -1,5 +1,84 @@
-(function(){
+'use strict';
 
-	console.log("Hello, world!");
-	
+(function(){
+	var elems = document.getElementsByClassName('header');
+
+	accordeon.onclick =  function(e) {
+
+		var target = e.target,
+			btns = document.getElementsByClassName('header'),
+			lists = document.getElementsByClassName('inside'),
+			triangle = target.firstElementChild.firstElementChild,
+			triangleDown = target.firstElementChild.lastElementChild,
+			triangles = document.getElementsByClassName('polygon-up'),
+			trianglesDown = document.getElementsByClassName('polygon-down'),
+			list = target.nextSibling.nextSibling,
+			start = Date.now();
+
+		for (var i = 0; i < btns.length; i++) {
+
+			btns[i].id = '';
+			triangles[i].style.display = 'none';
+			trianglesDown[i].style.display = 'block';
+			//lists[i].id = '';
+		};
+
+		target.id = 'active';
+		triangleDown.style.display = 'none';
+		triangle.style.display = 'block';
+		//target.nextSibling.nextSibling.id = 'insideActive';
+
+		var timer = setInterval(function() {
+
+			var timePassed = Date.now() - start;
+							
+			slideUp(timePassed).then(removeId()
+			).then(function() {
+			    console.log('Done!');
+			});
+
+			slide(timePassed);
+
+			if (timePassed >= 500) {
+				clearInterval(timer);
+				return;
+			}
+
+		}, 20);
+
+		function slide(timePassed) {
+
+			list.style.height = timePassed / 5 + 'px';
+		}
+
+		function slideUp(timePassed) {
+		    return new Promise(function(resolve){
+
+		    	for (var j = 0; j < lists.length; j++) {
+		    		if (lists[j].id === 'before') {
+
+		    			console.log(lists[j]);
+		    			console.log(lists[j].id);
+
+		    			lists[j].style.height = 100 - timePassed / 5 + 'px';
+		    		
+		    		}
+		    	}
+            
+            	resolve();
+        	});
+    	}
+
+    	function removeId() {
+		    return new Promise(function(resolve){
+		    	for (var j = 0; j < lists.length; j++) {
+		    		lists[j].id = '';
+		    	}
+            	resolve();
+        	});
+    	}
+
+		list.id = 'before';
+	}
+
 })();
